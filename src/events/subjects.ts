306754133c8e21a83991823d1bd8b887ccc2cb50
amaahov <amleshiba@gmail.com>
2023-09-03 04:41:39 +0200

    added order listeners

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -2,4 +2,7 @@ export enum Subjects {
   TicketCreated = 'ticket:created',
   TicketUpdated = 'ticket:updated',
   UserCreated = 'user:created',
+  UserUpdated = 'user:updated',
+  OrderCreated = 'order:created',
+  OrderCancelled = 'order:cancelled',
 }
